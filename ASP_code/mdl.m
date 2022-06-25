b647d5e678c5a5a9666fbe84298d27a862b055b7
KaiyueSun98 <94892075+KaiyueSun98@users.noreply.github.com>
2022-06-26 03:02:43 +0800

    Delete ASP_code directory

--- ASP_code/mdl.m
+++
@@ -1,8 +0,0 @@
-function [MDL, AIC,AICc] = mdl(x,p) % Turned into a function
-N = length(x);
-for i = 1:p
-    [~,E] = aryule(x,i);
-    MDL(i) = log10(E) + i*(log10(N)/N);
-    AIC(i) = log10(E) + 2*(i/N);
-    AICc(i)=AIC(i)+2*i*(i+1)/(N-i-1);
-end
--- ASP_code/pgm.m
+++
@@ -1,9 +0,0 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%3 Spectral Estimation and Modelling
-%1. pgm
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function P = pgm(x)
-    N = length(x);
-    xdft = fft(x);
-    P = (1/N).*(abs(xdft).^2);
-end
